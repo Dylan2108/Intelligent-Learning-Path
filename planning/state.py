23fef2c7b0140c700fde13d4bbbfb3c491a624c5
Dylan2108 <dylancabrera024@gmail.com>
2026-05-20 02:38:39 -0400

    Add user state

---
+++ planning/state.py
@@ -0,0 +1,9 @@
+class State:
+    def __init__(self, completed_courses, path, total_cost, total_time):
+        self.completed_courses = completed_courses
+        self.path = path
+        self.total_cost = total_cost
+        self.total_time = total_time
+    
+    def __lt__(self, other):
+        return self.total_time < other.total_time
